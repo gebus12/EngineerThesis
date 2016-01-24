3acffdc2cee9e6baf057404c25724aaab889fac9
kolulu <kornelia.buchczyk@gmail.com>
2016-01-24 17:45:28 +0100

    History displaying

--- EngineerCodeFirst/Models/History.cs
+++ EngineerCodeFirst/Models/History.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,17 @@ namespace EngineerCodeFirst.Models
     public class History
     {
         public int HistoryID { get; set; }
-        public int StartTime { get; set; }
-        public int EndTime { get; set; }
+        [Required(ErrorMessage = "Time required")]
+        [Display(Name = "Shift start time")]
+        public string StartTime { get; set; }
+        [Required(ErrorMessage = "Time required")]
+        [Display(Name = "Shift end time")]
+        public string EndTime { get; set; }
+        [Required(ErrorMessage = "Driver required")]
+        [Display(Name = "Driver")]
         public int DriverID { get; set; }
+        [Required(ErrorMessage = "Bus required")]
+        [Display(Name = "Bus")]
         public int BusID { get; set; }
         public virtual Driver Driver { get; set; }
         public virtual Bus Bus { get; set; }
